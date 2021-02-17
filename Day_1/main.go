6d1c44c9fe00f412fccc6d244d73e8bf979fe8fd
amirsarusi <42070364+amirsarusi@users.noreply.github.com>
2021-02-17 21:14:05 +0200

    Update main.go

--- Day_1/main.go
+++ Day_1/main.go
@@ -13,16 +13,8 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	is := getIntSlice(bs)
-	for i := 0; i < len(is); i++ {
-		for j := 1; j < len(is); j++ {
-			if is[i]+is[j] == 2020 {
-				fmt.Println("values are :", is[i], is[j], "when multiplied ", is[i]*is[j])
-				os.Exit(0)
-			}
-		}
-	}
-
+	//twoNumSol(getIntSlice(bs), 2020)
+	threeNumSol(getIntSlice(bs), 2020)
 }
 
 func getIntSlice(bs []byte) []int64 {
@@ -38,4 +30,31 @@ func getIntSlice(bs []byte) []int64 {
 		}
 	}
 	return is
+}
+
+func twoNumSol(is []int64, sumTarget int64) (product int64) {
+	for i := 0; i < len(is); i++ {
+		for j := 1; j < len(is); j++ {
+			if is[i]+is[j] == sumTarget {
+				fmt.Println("values are :", is[i], is[j], "when multiplied ", is[i]*is[j])
+				return is[i] * is[j]
+			}
+		}
+	}
+	return -1
+}
+
+func threeNumSol(is []int64, sumTarget int64) (product int64) {
+
+	for i := 0; i < len(is); i++ {
+		for j := i + 1; j < len(is); j++ {
+			for k := j + 1; k < len(is); k++ {
+				if is[i]+is[j]+is[k] == sumTarget && is[i] != 0 && is[j] != 0 && is[k] != 0 {
+					fmt.Println("values are :", is[i], is[j], is[k], "when multiplied ", is[i]*is[j]*is[k])
+					return is[i] * is[j] * is[k]
+				}
+			}
+		}
+	}
+	return -1
 }
