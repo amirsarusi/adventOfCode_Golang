46cd96fa0375a0592286399ce99ae9668fc5fe07
amirsarusi <42070364+amirsarusi@users.noreply.github.com>
2021-02-18 18:24:49 +0200

    Update main.go

--- Day_1/main.go
+++ Day_1/main.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	//twoNumSol(getIntSlice(bs), 2020)
+	twoNumSol(getIntSlice(bs), 2020)
 	threeNumSol(getIntSlice(bs), 2020)
 }
 
@@ -33,28 +33,30 @@ func getIntSlice(bs []byte) []int64 {
 }
 
 func twoNumSol(is []int64, sumTarget int64) (product int64) {
+	return findPair(0, sumTarget, is)
+}
+
+func threeNumSol(is []int64, sumTarget int64) (product int64) {
 	for i := 0; i < len(is); i++ {
-		for j := 1; j < len(is); j++ {
-			if is[i]+is[j] == sumTarget {
-				fmt.Println("values are :", is[i], is[j], "when multiplied ", is[i]*is[j])
-				return is[i] * is[j]
-			}
+		difInvoice := sumTarget - is[i]
+		found := findPair(int64(i), difInvoice, is)
+		if found > 0 {
+			fmt.Println(found * is[i])
+			return found * is[i]
 		}
 	}
 	return -1
 }
 
-func threeNumSol(is []int64, sumTarget int64) (product int64) {
-
-	for i := 0; i < len(is); i++ {
-		for j := i + 1; j < len(is); j++ {
-			for k := j + 1; k < len(is); k++ {
-				if is[i]+is[j]+is[k] == sumTarget && is[i] != 0 && is[j] != 0 && is[k] != 0 {
-					fmt.Println("values are :", is[i], is[j], is[k], "when multiplied ", is[i]*is[j]*is[k])
-					return is[i] * is[j] * is[k]
-				}
-			}
+func findPair(index int64, target int64, is []int64) int64 {
+	m := make(map[int64]bool)
+	for i := index; int(i) < len(is); i++ {
+		currentVal := is[i]
+		differenceVal := target - currentVal
+		if m[differenceVal] {
+			return currentVal * differenceVal
 		}
+		m[currentVal] = true
 	}
 	return -1
 }
